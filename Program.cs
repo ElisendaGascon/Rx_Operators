7c49dd44ad906959f96862a0c5edb4129ea53160
ElisendaGascon <elisendagascon@icloud.com>
2022-12-01 16:43:35 +0000

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Rx_Operators;
+using System.Reactive.Linq;
+
+Console.WriteLine("Hello, World!");
+
+IObservable<long> ticks = Observable.Timer(TimeSpan.Zero, TimeSpan.FromSeconds(1)).Take(5);
+
+IObservable<long> filteredTicks = new WhereOperator<long>(ticks, value => value % 2 == 1);
+
+IDisposable sub = filteredTicks.Subscribe(
+    tickCount => Console.WriteLine(tickCount),
+    () => Console.WriteLine("Done"));
+Console.ReadLine();
+sub.Dispose();
+Console.WriteLine("Unsubscribed");
+Console.ReadLine();
---
+++ WhereOperator.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Rx_Operators
+{
+    internal class WhereOperator<T> : IObservable<T>
+    {
+        private readonly IObservable<T> source;
+        private readonly Func<T, bool> filter;
+
+        public WhereOperator(IObservable<T> source, Func<T, bool> filter)
+        {
+            this.source = source;
+            this.filter = filter;
+        }
+
+        public IDisposable Subscribe(IObserver<T> observer)
+        {
+            var op = new Implementation(observer, this.source, this.filter);
+            return op;
+        }
+
+        private class Implementation : IObserver<T>, IDisposable
+        {
+            private IObserver<T> observer;
+            private readonly Func<T, bool> filter;
+            private IDisposable sub;
+
+            public Implementation(IObserver<T> observer, IObservable<T> source, Func<T, bool> filter)
+            {
+                this.observer = observer;
+                this.filter = filter;
+                this.sub = source.Subscribe(this);
+            }
+
+            public void Dispose()
+            {
+                this.sub.Dispose();
+            }
+
+            public void OnCompleted()
+            {
+                observer.OnCompleted();
+            }
+
+            public void OnError(Exception error)
+            {
+                observer.OnError(error);
+            }
+
+            public void OnNext(T value)
+            {
+                if( this.filter(value))
+                {
+                    observer.OnNext(value);
+                }
+            }
+        }
+    }
+}
