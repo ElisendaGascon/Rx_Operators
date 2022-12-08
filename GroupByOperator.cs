549756792d2385d1d29f6c6e7dae92ae43ffff43
ElisendaGascon <elisendagascon@icloud.com>
2022-12-08 16:41:04 +0000

    Add GroupBy operator

---
+++ GroupByOperator.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reactive.Linq;
+using System.Reactive.Subjects;
+using System.Runtime;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Rx_Operators
+{
+    internal class GroupByOperator<TKey, TItem> : IObservable<IGroupedObservable<TKey, TItem>>
+    {
+        private readonly IObservable<TItem> source;
+        private readonly Func<TItem, TKey> keySelector;
+
+        public GroupByOperator(IObservable<TItem> source, Func<TItem, TKey> keySelector)
+        {
+            this.source = source;
+            this.keySelector = keySelector;
+        }
+
+        public IDisposable Subscribe(IObserver<IGroupedObservable<TKey, TItem>> observer)
+        {
+            return new Implementation(observer, this.source, this.keySelector);
+        }
+
+        private class Implementation : IObserver<TItem>, IDisposable
+        {
+            private IObserver<IGroupedObservable<TKey, TItem>> observer;
+            private IObservable<TItem> source;
+            private Func<TItem, TKey> keySelector;
+            private IDisposable sub;
+            private Dictionary<TKey, Group> groups = new();
+
+            public Implementation(IObserver<IGroupedObservable<TKey, TItem>> observer, IObservable<TItem> source, Func<TItem, TKey> keySelector)
+            {
+                this.observer = observer;
+                this.source = source;
+                this.keySelector = keySelector;
+                this.sub = source.Subscribe(this);
+            }
+
+            public class Group : IGroupedObservable<TKey, TItem>
+            {
+                private readonly Subject<TItem> subject = new();
+                public Group(TKey key)
+                {
+                    this.Key = key;
+                }
+
+                public void OnNext(TItem value)
+                {
+                    this.subject.OnNext(value);
+                }
+
+                public TKey Key { get; }
+
+                public IDisposable Subscribe(IObserver<TItem> observer)
+                {
+                    return this.subject.Subscribe(observer);
+                }
+
+                internal void OnCompleted()
+                {
+                    this.subject.OnCompleted();
+                }
+            }
+
+            public void Dispose()
+            {
+                this.sub.Dispose();
+            }
+
+            public void OnCompleted()
+            {
+                foreach( (_, Group group) in this.groups)
+                {
+                    group.OnCompleted();
+                }
+            }
+
+            public void OnError(Exception error)
+            {
+                throw new NotImplementedException();
+            }
+
+            public void OnNext(TItem value)
+            {
+                
+                TKey groupKey = keySelector(value);
+                if (!groups.TryGetValue(groupKey, out Group? group))
+                {
+                    group = new Group(groupKey);
+                    groups.Add(groupKey, group);
+                    this.observer.OnNext(group);
+                }
+                group.OnNext(value);
+            }
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -4,13 +4,26 @@ using System.Reactive.Linq;
 
 Console.WriteLine("Hello, World!");
 
+int[] numbers = { 1, 2, 3, 4, 5, 6 };
+var groupedNumbers = numbers.GroupBy(x => x % 3).ToArray();
+
 IObservable<long> ticks = Observable.Timer(TimeSpan.Zero, TimeSpan.FromSeconds(1)).Take(5);
 
 IObservable<long> filteredTicks = new WhereOperator<long>(ticks, value => value % 2 == 1);
+IObservable<IGroupedObservable<long, long>> groupedTicks = new GroupByOperator<long, long>(ticks, value => value % 3);
 
-IDisposable sub = filteredTicks.Subscribe(
+/*IDisposable sub = filteredTicks.Subscribe(
     tickCount => Console.WriteLine(tickCount),
-    () => Console.WriteLine("Done"));
+    () => Console.WriteLine("Done"));*/
+IDisposable sub = groupedTicks.Subscribe(
+    group =>
+    {
+        Console.WriteLine($"group {group.Key}");
+        group.Subscribe(
+            tickCount => Console.WriteLine($"Group {group.Key} produced {tickCount}"),
+            () => Console.WriteLine($"Group {group.Key} ended"));
+    });
+
 Console.ReadLine();
 sub.Dispose();
 Console.WriteLine("Unsubscribed");
